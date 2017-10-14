094c4d57f3a661912a21caf182befb4d40bc27d2
Jakub Ławicki <jakub.lawicki@gmail.com>
2017-10-14 11:15:14 +0200

    Some renaming for OCD.

--- Source/main.js
+++ Source/main.js
@@ -1,28 +1,26 @@
 
-var gGSM;
+var gsm;
 
 function setup() {
   scoreElem = createDiv('Score = 0');
   scoreElem.position(20, 20);
   scoreElem.id = 'score';
   scoreElem.style('color', 'white');
-  
+
   createCanvas(500, 500);
   frameRate(15);
-  stroke(255);
-  strokeWeight(10);
-  
-  gGSM = new GameStateManager();
-  var gs = new GameState_MainMenu();
-  var gameplay = new GameState_Gameplay();
-  gGSM.pushState(gameplay);
+
+  gsm = new GameStateManager();
+  var gsMainMenu = new GameState_MainMenu();
+  var gsGameplay = new GameState_Gameplay();
+  gsm.pushState(gsGameplay);
 }
 
 function draw() {
   background(0);
-  
-  gGSM.tick(1/15);
-  
+
+  gsm.tick(1/15);
+
   xFruit = floor(random(10, (width - 100) / 10)) * 10;
   yFruit = floor(random(10, (height - 100) / 10)) * 10;
   point(xFruit, yFruit);
