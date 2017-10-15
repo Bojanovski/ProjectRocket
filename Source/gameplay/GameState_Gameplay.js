658e07756f64558a81f43c8c281d2c3ae3fd16e5
Jakub Ławicki <jakub.lawicki@gmail.com>
2017-10-15 02:03:15 +0200

    Not working.

--- Source/gameplay/GameState_Gameplay.js
+++ Source/gameplay/GameState_Gameplay.js
@@ -9,12 +9,21 @@ class GameState_Gameplay extends GameState {
 
 		this.physicsEngine = new PhysicsEngine();
 		this.shipManager = new ShipManager();
-		this.level = new Level(this.seed);
+		this.ships = [];
 	}
 
-	initiate() {
-		this.ship = this.shipManager.defaultShip;
-		this.ship.initiate(random(10000));
+	startSimulation(shipPrototype, numberOfShips) {
+
+		// build level
+		this.level = new Level(this.seed);
+
+		// spawn ships
+		this.ships = [];
+		for (var i = 0; i < numberOfShips; i++) {
+			var ship = _.cloneDeep(shipPrototype);
+			ship.initiate(random(10000));
+			this.ships.push(ship);
+		}
 
 		// Create static colliders for rocks
 		for (var i = 0; i < this.level.rocks.length; i++) {
@@ -23,22 +32,27 @@ class GameState_Gameplay extends GameState {
 		}
 
 		// Create a particle for every node in the ship.
-		if (this.ship.nodes !== undefined) {
-			for (var i = 0; i < this.ship.nodes.length; i++) {
+		for (var i = 0; i < this.ships.length; i++) {
+			if (this.ships[i] !== undefined && this.ships[i].nodes !== undefined) {
+					for (var i = 0; i < this.ships[i].nodes.length; i++) {
 
-				var node = this.ship.nodes[i];
-				var posX = node.x;
-				var posY = node.y;
-				var newParticle = new Particle(posX, posY, node.r);
-				this.physicsEngine.particles.push(newParticle);
-				this.ship.particleNodeMap[node.id] = newParticle;
+					var node = this.ships[i].nodes[i];
+					var posX = node.x;
+					var posY = node.y;
+					var newParticle = new Particle(posX, posY, node.r);
+					this.physicsEngine.particles.push(newParticle);
+					this.ships[i].particleNodeMap[node.id] = newParticle;
 
-				if (node.isThruster()) { // ship needs to apply forces to thrusters (the actual thrust).
-					this.ship.thrusterParticles[node.id] = newParticle;
-
+					if (node.isThruster()) { // ship needs to apply forces to thrusters (the actual thrust).
+						this.ships[i].thrusterParticles[node.id] = newParticle;
+					}
 				}
 			}
 		}
+	}
+
+	initiate() {
+		this.startSimulation(this.shipManager.defaultShip, 1);
 	}
 
 	deinitiate() {
@@ -52,13 +66,18 @@ class GameState_Gameplay extends GameState {
 		// Update the physics.
 		this.physicsEngine.update(deltaTime);
 
-		// Update the ship.
-		this.ship.update(deltaTime);
+		// Update the ship
+		for (var i = 0; i < this.ships.length; i++) {
+			this.ships[i].update(deltaTime);
+		}
 	}
 
 	display() {
 		this.level.display();
-		this.ship.display();
+
+		for (var i = 0; i < this.ships.length; i++) {
+			this.ships[i].display();
+		}
 	}
 
 	print() {
